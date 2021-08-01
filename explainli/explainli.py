753b765f877771e41cdc5b2dd91e5e8dfe6bb46d
KeremZaman <kerem.zaman@boun.edu.tr>
2021-08-02 00:58:54 +0300

    Make Shapley and LIME evaluate instances in batches single by single due to sampling issues

--- explainli/explainli.py
+++ explainli/explainli.py
@@ -102,8 +102,9 @@ class NLIAttribution(object):
 
         :return:
         """
-        if self.config.forward_scoring is ForwardScoringOptions.LOSS and self.config.attribution_method in \
-                [AttributionMethods.Occlusion, AttributionMethods.DeepLift, AttributionMethods.Shapley]:
+        if (self.config.forward_scoring is ForwardScoringOptions.LOSS and self.config.attribution_method in \
+                [AttributionMethods.Occlusion, AttributionMethods.DeepLift]) or self.config.attribution_method in \
+                [AttributionMethods.Shapley, AttributionMethods.LIME]:
             return True
 
         return False
